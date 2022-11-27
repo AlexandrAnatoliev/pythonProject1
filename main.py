941b1e3191da0dd9b465a69f93f4983b75528fa5
AlexandrAnatoliev <per-1986@list.ru>
2022-11-27 22:00:11 +0300

    посылаются случайные шутки с периодом 30 секунд

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 # Файл с анекдотами должен лежать в папке data рядом со скриптом бота.
 
 import telebot
+import random
 import time
 from config import token, channel
 
@@ -21,9 +22,8 @@ f = open('fun.txt', 'r', encoding='UTF-8')
 jokes = f.read().split('\n')
 f.close()
 
-# Пока не закончатся шутки, посылаем их в канал
-for joke in jokes:
-    bot.send_message(CHANNEL_NAME, joke)
-    # Делаем паузу в один час
-    time.sleep(3600)
-bot.send_message(CHANNEL_NAME, "Анекдоты закончились :-(")
+# посылаются случайные шутки с периодом 30 секунд
+bot.send_message(CHANNEL_NAME, random.choice(jokes))
+time.sleep(30)
+bot.send_message(CHANNEL_NAME, random.choice(jokes))
+
